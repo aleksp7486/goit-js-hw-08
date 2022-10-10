6059682f30a4a14b95fbc72a2a5156795f91c306
aleksp7468 <aleksandrpopov7486@gmail.com>
2022-10-10 07:40:27 +0300

    add gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+'Use strict';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items.js';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const gallery = document.querySelector('.gallery');
+const galleryListItems = createGalleryList(galleryItems);
+gallery.append(galleryListItems);
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGalleryList(arr) {
+  const galleryList = document.createElement('ul');
+  galleryList.classList.add('gallery__list');
+
+  const galleryItems = arr.map(elem => {
+    const { preview, original, description } = elem;
+    const galleryItem = document.createElement('li');
+    galleryItem.classList.add('gallery__item');
+    const galleryLink = document.createElement('a');
+    galleryLink.classList.add('gallery__link');
+    galleryLink.setAttribute('href', original);
+    galleryItem.append(galleryLink);
+    const galleryImage = document.createElement('img');
+    galleryImage.classList.add('gallery__image');
+    galleryImage.setAttribute('src', preview);
+    galleryImage.setAttribute('alt', description);
+    galleryLink.append(galleryImage);
+    return galleryItem;
+  });
+
+  galleryList.append(...galleryItems);
+  return galleryList;
+}
